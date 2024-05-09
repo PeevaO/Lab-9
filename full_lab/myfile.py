fbfb0d41de6899853a65c6d7c1952914ff21b791
PeevaO <peeva.olesya@mail.ru>
2024-05-09 15:26:34 +0300

    myfile.py

--- full_lab/myfile.py
+++ full_lab/myfile.py
@@ -7,14 +7,12 @@ app = Flask('Places of work')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///companies.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-exper = 1
 
 
 class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comp_name = db.Column(db.String(300))
     term = db.Column(db.Integer)
-    in_stock = db.Column(db.Boolean, default=True)
 
     def __repr__(self):
         return f'Company{self.id}. {self.comp_name} - {self.term} mon.'
